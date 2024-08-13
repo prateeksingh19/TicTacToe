06d1db2c8be5c158314247ec562d38740b43bb4a
Prateek Singh <prateekvns99@gmail.com>
2024-08-13 15:31:14 +0530

    completed frontend part

--- components/board.tsx
+++ components/board.tsx
@@ -1,22 +1,109 @@
+"use client";
+import { useCallback, useState } from "react";
 import Square from "./square";
 
 export default function Board() {
+  const [board, setBoard] = useState<string[]>(Array(9).fill(null));
+  const [xTurn, setXTurn] = useState(true);
+  const [playAI, setPlayAI] = useState(true);
+  const winner = calculateWinner(board);
+  const status = winner
+    ? `Winner: ${winner}`
+    : `Next player: ${xTurn ? "X" : "O"}`;
+
+  function handlePlayerMove(index: number) {
+    if (board[index] || winner) return;
+
+    const currentBoard = [...board];
+    currentBoard[index] = xTurn ? "X" : "O";
+
+    setBoard(currentBoard);
+    setXTurn((prevXTurn) => {
+      const nextXTurn = !prevXTurn;
+      if (!nextXTurn && playAI) {
+        setTimeout(() => randomAIMove(currentBoard), 500);
+      }
+      return nextXTurn;
+    });
+  }
+
+  function randomAIMove(currentBoard: string[]) {
+    const squaresLeft = currentBoard
+      .map((value, index) => (value === null ? index : null))
+      .filter((index) => index !== null) as number[];
+
+    if (squaresLeft.length === 0 || calculateWinner(currentBoard)) return;
+
+    const randomIndex =
+      squaresLeft[Math.floor(Math.random() * squaresLeft.length)];
+
+    const newBoard = [...currentBoard];
+    newBoard[randomIndex] = "O";
+
+    setBoard(newBoard);
+    setXTurn(true);
+  }
+
+  function calculateWinner(board: string[]) {
+    const winningLines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+
+    for (let lines of winningLines) {
+      const [a, b, c] = lines;
+
+      if (board[a] !== null && board[a] === board[b] && board[a] === board[c]) {
+        return board[a];
+      }
+    }
+    return null;
+  }
   return (
-    <div>
-      <div className="flex">
-        <Square value="x" />
-        <Square value="x" />
-        <Square value="x" />
+    <div className="w-80">
+      <div className="m-4 flex justify-center">
+        <div className="bg-blue-500 text-white font-semibold py-2 px-4 rounded shadow-md focus:outline-none focus:ring-2 focus:ring-red-300">
+          {status}
+        </div>
       </div>
-      <div className="flex">
-        <Square value="x" />
-        <Square value="x" />
-        <Square value="x" />
+      {playAI && (
+        <div className="m-4 flex justify-center">
+          <button
+            className="bg-blue-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded shadow-md focus:outline-none focus:ring-2 focus:ring-red-300"
+            onClick={() => {
+              setPlayAI(false);
+            }}
+          >
+            Play without AI
+          </button>
+        </div>
+      )}
+      <div className="grid grid-cols-3">
+        {board.map((value, index) => (
+          <Square
+            key={index}
+            value={value}
+            onClick={() => handlePlayerMove(index)}
+          />
+        ))}
       </div>
-      <div className="flex">
-        <Square value="x" />
-        <Square value="x" />
-        <Square value="x" />
+      <div className="m-4 flex justify-center">
+        <button
+          className="bg-blue-500 hover:bg-red-600 text-white font-semibold py-2 px-4 rounded shadow-md focus:outline-none focus:ring-2 focus:ring-red-300"
+          onClick={() => {
+            setXTurn(true);
+            setPlayAI(true);
+            setBoard(Array(9).fill(null));
+          }}
+        >
+          Reset
+        </button>
       </div>
     </div>
   );
--- components/square.tsx
+++ components/square.tsx
@@ -1,17 +1,17 @@
-"use client";
-import { useState } from "react";
-
-export default function Square({ value }: { value: string }) {
-  const [bool, setBool] = useState(true);
+export default function Square({
+  value,
+  onClick,
+}: {
+  value: string | null;
+  onClick: () => void;
+}) {
   return (
     <div className="m-2">
       <button
         className="w-24 h-24 bg-gray-200 border-2 border-black flex items-center justify-center text-4xl font-bold cursor-pointer hover:bg-gray-300 transition-colors"
-        onClick={() => {
-          setBool((prevValue) => !prevValue);
-        }}
+        onClick={onClick}
       >
-        {bool ? "X" : "O"}
+        {value}
       </button>
     </div>
   );
